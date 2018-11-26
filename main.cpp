e98fdf4df67b3066aa86bd8a1399b9673c17c408
Hieracon <37873846+Hieracon@users.noreply.github.com>
2018-11-26 17:26:38 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include "Prim.h"
+#include "Graph.h"
+
+int main() {
+	//-----------------------------------------Loading and Printing the Graph------------------------------
+	graph *G;
+
+	FILE* f;
+	fopen_s(&f,"cost.txt", "r");
+	G = loadGraph(f);
+
+	printf("\n Edges and Costs: \n");
+
+	for (int i = 0; i < G->noOfEdges; i++) {
+		printf("[%d <-> %d] --- cost = %d \n", G->pEdge[i].source, G->pEdge[i].destination, G->pEdge[i].weight);
+	}
+	//------------------------------------------------END---------------------------------------------------
+	//-----------------------------------------------PRIM---------------------------------------------------
+	int V[50][50], Q[50], H[50], n = 0, r = 0, k = 0;			//----|  Data for Prim's Alg
+	// ce reprezinta???
+
+	k = prim(V, Q, H, n, r, G);
+
+	printf("\n Prim: \n");
+
+	displayMST(H, n);
+
+	printf("\n MST cost = %d \n", k);
+	//-----------------------------------------------END-----------------------------------------------------
+	system("pause");
+
+}
